60d39d7840880def1d2ffd61e603c3bc6c7113c5
Aaron Brown <aaron.kaoru.brown@gmail.com>
2014-05-16 13:40:06 -0700

    More adjustments, footer is at very bottom now

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Boilerplate for Node Express website' });
 });
 
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'About Me' });
+});
+
 module.exports = router;
