43b3cbe5c0e134a02f6131ad14e97c91421fdab5
Aaron Brown <aaron.kaoru.brown@gmail.com>
2014-06-23 03:59:26 -0700

    Update splash page title, spelling consistency, and add link hover states on second CSS file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Aaron Brown' });
+  res.render('index', { title: 'Aaron Brown, front end web developer' });
 });
 
 router.get('/about', function(req, res) {
